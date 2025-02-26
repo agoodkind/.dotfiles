17a933eff706fd2e2986eb64650ba4fe4ad61960
Alexander Goodkind <alexander.goodkind@chime.com>
2025-02-25 21:49:28 -0800

    Refactor dotfiles structure and add installation scripts
    - Remove Powerlevel10k theme and .p10k.zsh configuration
    - Add new install.sh and repair.sh scripts for system setup
    - Move configuration files to home/ directory
    - Update README.md with new plugin installation instructions
    - Add platform-specific installation scripts for macOS and Linux
    - Remove old setup.zsh script
    - Add .gitignore_global and IntelliJ IDEA project files

--- .vimrc
+++
@@ -1,6 +0,0 @@
-set hlsearch
-colorscheme koehler
-syntax on
-set ignorecase
-set smartcase
-set wildmenu
--- lib/head.sh
+++ lib/head.sh
@@ -1,8 +1,7 @@
+#!/usr/bin/env bash
+
 ##############################################
 # Platform-independent omz/zsh configuration #
 # do not modify this section                 #
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
 export ZSH="$DOTDOTFILES/lib/.oh-my-zsh"
 ZSH_CUSTOM="$DOTDOTFILES/lib/omz-custom"
