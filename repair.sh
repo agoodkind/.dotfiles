4ddd446b8def7e834cf1fb2de9add3e1ba9ca0eb
Alex Goodkind <alex@goodkind.io>
2025-02-25 23:19:15 -0800

    Enhance dotfiles configuration and repair script
    - Update repair.sh with improved logging and symlink creation
    - Add .p10k.zsh configuration for Powerlevel10k theme
    - Modify .zshrc to conditionally source .p10k.zsh
    - Update .gitignore to remove duplicate .DS_Store entry
    - Improve script readability and error handling
    - Add KeyCastr and Speedtest CLI to Homebrew installations

--- repair.sh
+++ repair.sh
@@ -7,7 +7,7 @@ timestamp=$(date +"%Y%m%d_%H%M%S")
 
 export DOTDOTFILES="$(dirname "$(readlink -f "$0")")"
 
-echo "DOTDOTFILES: $DOTDOTFILES"
+printf "DOTDOTFILES: %s\n" "$DOTDOTFILES"
 
 BACKUPS_PATH="$DOTDOTFILES/backups"
 mkdir -p "$BACKUPS_PATH"
@@ -15,17 +15,24 @@ mkdir -p "$BACKUPS_PATH"
 # go through all files in $DOTDOTFILES/home and create symlinks in $HOME
 # make a backup of each file if it exists
 files=$(find "$DOTDOTFILES/home" -type f)
-for file in $files; do
-    echo "Creating symlink from $file to $HOME/$(basename "$file")"
-    if [ -f "$HOME/$(basename "$file")" ]; then
-        echo "Backing up $HOME/$(basename "$file") to $BACKUPS_PATH/.$(basename "$file").bak-$timestamp"
-        mv "$HOME/$(basename "$file")" "$BACKUPS_PATH/.$(basename "$file").bak-$timestamp"
+for source_file in $files; do
+    file_name=$(basename "$source_file")
+    home_file=$HOME/$file_name
+    
+    printf "\n\n"
+
+    if [ -a "$home_file" ]; then
+        backup_file="$BACKUPS_PATH/$file_name.bak-$timestamp"
+        printf "Backing up %s to %s\n" "$home_file" "$backup_file"
+        mv "$home_file" "$backup_file"
     fi
-    ln -sF "$file" "$HOME/$(basename "$file")"
+
+    printf "Creating symlink from %s to %s\n" "$source_file" "$home_file"
+    ln -sfv "$source_file" "$home_file"
 done
 
-echo "Updating plugins and submodules"
+printf "\nUpdating plugins and submodules\n"
 git submodule update --init --recursive
 
 
-echo "Run 'source \"$HOME/.zshrc\"' to apply changes or restart your terminal"
+printf "\nRun 'source \"%s/.zshrc\"' to apply changes or restart your terminal\n" "$HOME"
