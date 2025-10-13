24c4cf23091bd04e91e5693c48a41e008de14e65
Alex Goodkind <alex@goodkind.io>
2025-10-12 19:00:25 -0700

    Add vault host configuration and update apt.sh for email tools

--- lib/install/apt.sh
+++ lib/install/apt.sh
@@ -6,14 +6,20 @@ curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh 
 # install github cli
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+
+sudo add-apt-repository ppa:sasl-xoauth2/stable -y
+
 sudo apt update -y
 sudo apt upgrade -y
+
+# install email tools
+sudo apt install sasl-xoauth2 postfix -y
 
 # install tools
 sudo apt install gh tree ack ack-grep -y
 
 # System utilities
-sudo apt install speedtest-cli moreutils bash grep rename aria2 sendmail -y
+sudo apt install speedtest-cli moreutils bash grep rename aria2 -y
 
 # Shell and terminal tools
 sudo apt install zsh thefuck fzf eza neovim -y
@@ -22,7 +28,7 @@ sudo apt install zsh thefuck fzf eza neovim -y
 sudo apt install pandoc grc less most -y
 
 # install languages
-sudo apt install python3 ruby go -y
+sudo apt install python3 ruby go nodejs -y
 
 # install bat and link to ~/.local/bin (to avoid conflicts)
 sudo apt install bat -y
