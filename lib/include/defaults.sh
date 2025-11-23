77525471be2edc4c609ed72cebca21f4ff3b0fb4
Alex Goodkind <alex@goodkind.io>
2025-11-23 13:47:50 -0800

    mapping fixes

--- lib/include/defaults.sh
+++ lib/include/defaults.sh
@@ -62,15 +62,15 @@ run_with_defaults() {
 # print [DEBUG] message if DEBUG is true
 # Usage: debug_echo "message"
 debug_echo() {
-    if [[ -f ~/.cache/dotfiles_debug_enabled ]] || [[ "${DEBUG:-false}" == "true" ]]; then
+    if [[ -f "$HOME/.cache/dotfiles_debug_enabled" ]] || [[ "${DEBUG:-false}" == "true" ]]; then
         printf "%b[DEBUG]%b %s%b\n" "${GRAY}" "${NC}" "$*" "${NC}"
     fi
 }
 
 enable_debug() {
-    touch ~/.cache/dotfiles_debug_enabled
+    touch "$HOME/.cache/dotfiles_debug_enabled"
 }
 
 disable_debug() {
-    rm -f ~/.cache/dotfiles_debug_enabled
+    rm -f "$HOME/.cache/dotfiles_debug_enabled"
 }
